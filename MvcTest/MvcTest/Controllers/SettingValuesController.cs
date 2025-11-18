560d0834d41a78d527b331d59358d869f95eda36
H Movaghari <hmovaghari@gmail.com>
2025-11-18 21:35:45 +0330

    Add SettingValueRepository

--- MvcTest/MvcTest/Controllers/SettingValuesController.cs
+++ MvcTest/MvcTest/Controllers/SettingValuesController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using MvcTest.Repository;
 using MyAccounting.Data;
 using MyAccounting.ViewModels;
 
@@ -8,8 +8,11 @@ namespace MvcTest.Controllers
 {
     public class SettingValuesController : BaseAuthorizeController
     {
+        private SettingValueRepository _settingValueRepository;
+
         public SettingValuesController(SqlDBContext context) : base(context)
         {
+            _settingValueRepository = new SettingValueRepository(context);
         }
 
         // GET: Users
@@ -21,7 +24,7 @@ namespace MvcTest.Controllers
             {
                 return RedirectToMainPage();
             }
-            var settingsValuesDTO = await GetsettingsValuesDTO(user);
+            var settingsValuesDTO = await _settingValueRepository.GetsettingsValuesDTO(user);
             if (settingsValuesDTO == null)
             {
                 return NotFound();
@@ -29,26 +32,7 @@ namespace MvcTest.Controllers
             return View(settingsValuesDTO);
         }
 
-        private async Task<List<SettingValueDTO>> GetsettingsValuesDTO(UserDTO user)
-        {
-            var settingsKeys = await _context.SettingKeys.ToListAsync();
-            var settingsValues = await _context.SettingValues.Where(sv => sv.UserID == user.UserID).ToListAsync();
-            var settingsValuesDTO = new List<SettingValueDTO>();
-            settingsKeys.ForEach(key =>
-            {
-                var existingValue = settingsValues.FirstOrDefault(sv => sv.SettingKeyID == key.SettingKeyID);
-                settingsValuesDTO.Add(new SettingValueDTO
-                {
-                    SettingValueID = existingValue != null ? existingValue.SettingValueID : Guid.NewGuid(),
-                    SettingKeyID = key.SettingKeyID,
-                    SettingKeyTitle = key.Key,
-                    SettingValueDescription = key.Description,
-                    UserID = user.UserID,
-                    SettingValue = existingValue != null ? existingValue.Value : string.Empty
-                });
-            });
-            return settingsValuesDTO;
-        }
+        
 
         [Authorize]
         public async Task<IActionResult> AddOrUpdate()
@@ -59,7 +43,7 @@ namespace MvcTest.Controllers
                 return RedirectToMainPage();
             }
 
-            var settingValueDTOs = await GetsettingsValuesDTO(user);
+            var settingValueDTOs = await _settingValueRepository.GetsettingsValuesDTO(user);
 
             if (settingValueDTOs == null)
             {
@@ -80,35 +64,7 @@ namespace MvcTest.Controllers
 
             if (ModelState.IsValid)
             {
-                var userID = settingValueDTOs?.First().UserID;
-
-                // استفاده از Task.WhenAll به جای ForEach با async
-                // settingValueDTOs?.ForEach(async dto =>
-                var tasks = settingValueDTOs?.Select(async dto =>
-                {
-                    var existingSettingValue = await _context.SettingValues
-                        .FirstOrDefaultAsync(sv => sv.UserID == userID && sv.SettingKeyID == dto.SettingKeyID);
-                    
-                    if (existingSettingValue != null)
-                    {
-                        existingSettingValue.Value = dto.SettingValue;
-                        _context.Update(existingSettingValue);
-                    }
-                    else
-                    {
-                        var newSettingValue = new MyAccounting.Data.Model.SettingValue
-                        {
-                            SettingValueID = dto.SettingValueID,
-                            SettingKeyID = dto.SettingKeyID,
-                            UserID = dto.UserID,
-                            Value = dto.SettingValue
-                        };
-                        _context.Add(newSettingValue);
-                    }
-                }) ?? new List<Task>();
-
-                await Task.WhenAll(tasks);
-                await _context.SaveChangesAsync();
+                _ = _settingValueRepository.AddOrUpdate(settingValueDTOs);
 
                 return RedirectToAction(nameof(Index));
             }
---
+++ MvcTest/MvcTest/Repository/SettingValueRepository.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcTest.Controllers;
+using MyAccounting.Data;
+using MyAccounting.Data.Model;
+using MyAccounting.ViewModels;
+
+namespace MvcTest.Repository
+{
+    public class SettingValueRepository
+    {
+        private SqlDBContext _context;
+
+        public SettingValueRepository(SqlDBContext contex)
+        {
+            _context = contex;
+        }
+
+        public async Task<List<SettingValueDTO>?> GetsettingsValuesDTO(UserDTO user)
+        {
+            try
+            {
+                var settingsKeys = await _context.SettingKeys.ToListAsync();
+                var settingsValues = await _context.SettingValues.Where(sv => sv.UserID == user.UserID).ToListAsync();
+                var settingsValuesDTO = new List<SettingValueDTO>();
+                settingsKeys.ForEach(key =>
+                {
+                    var existingValue = settingsValues.FirstOrDefault(sv => sv.SettingKeyID == key.SettingKeyID);
+                    settingsValuesDTO.Add(new SettingValueDTO
+                    {
+                        SettingValueID = existingValue != null ? existingValue.SettingValueID : Guid.NewGuid(),
+                        SettingKeyID = key.SettingKeyID,
+                        SettingKeyTitle = key.Key,
+                        SettingValueDescription = key.Description,
+                        UserID = user.UserID,
+                        SettingValue = existingValue != null ? existingValue.Value : string.Empty
+                    });
+                });
+                return settingsValuesDTO;
+            }
+            catch (Exception ex)
+            {
+                ErrorLogRepository.SaveErrorLog(_context, ex, nameof(SettingValueRepository), nameof(GetsettingsValuesDTO), user);
+            }
+
+            return null;
+        }
+
+        public async Task<bool?> AddOrUpdate(List<SettingValueDTO> settingValueDTOs)
+        {
+            try
+            {
+                var userID = settingValueDTOs?.First().UserID;
+
+                // استفاده از Task.WhenAll به جای ForEach با async
+                // settingValueDTOs?.ForEach(async dto =>
+                var tasks = settingValueDTOs?.Select(async dto =>
+                {
+                    var existingSettingValue = await _context.SettingValues
+                        .FirstOrDefaultAsync(sv => sv.UserID == userID && sv.SettingKeyID == dto.SettingKeyID);
+
+                    if (existingSettingValue != null)
+                    {
+                        existingSettingValue.Value = dto.SettingValue;
+                        _context.Update(existingSettingValue);
+                    }
+                    else
+                    {
+                        var newSettingValue = new SettingValue
+                        {
+                            SettingValueID = dto.SettingValueID,
+                            SettingKeyID = dto.SettingKeyID,
+                            UserID = dto.UserID,
+                            Value = dto.SettingValue
+                        };
+                        _context.Add(newSettingValue);
+                    }
+                }) ?? new List<Task>();
+
+                await Task.WhenAll(tasks);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ErrorLogRepository.SaveErrorLog(_context, ex, nameof(SettingValueRepository), nameof(AddOrUpdate), settingValueDTOs);
+            }
+
+            return null;
+        }
+    }
+}
